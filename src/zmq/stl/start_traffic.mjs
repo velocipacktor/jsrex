bdcd8c21412c98c91a0f2888484c9832047cb7e3
Edwin Pers <pers.edwin@gmail.com>
2022-08-21 13:47:04 -0400

    stl start_traffic() function

---
+++ src/zmq/stl/start_traffic.mjs
@@ -0,0 +1,29 @@
+'use strict';
+
+// Stateless version of "Start traffic"
+// https://trex-tgn.cisco.com/trex/doc/trex_rpc_server_spec.html#_start_traffic
+
+export async function start_traffic({
+  api_h = this.api_h,
+  user = this.user,
+  handler = this.port_handler,
+  duration = 30,
+  multiplier = 1,
+  core_mask = 0xffffffff, // Mask of client ports. bit is 0 ⇒ client is disabled.
+  profile_id = '_', // Use default profile if none specified
+} = {}) {
+  const response = await this.send('start_traffic', {
+    api_h: api_h,
+    user: user,
+    handler: handler,
+    profile_id: profile_id,
+    duration: duration,
+    core_mask: core_mask,
+    mul: {
+      op: 'abs',
+      type: 'raw',
+      value: multiplier,
+    },
+  });
+  return response;
+}
