f9261fdc004ef00dadfcd6b967502f4ae6134d93
Edwin Pers <pers.edwin@gmail.com>
2022-08-22 16:22:20 -0400

    additional params for get_profile_list()

--- src/zmq/astf/get_profile_list.mjs
+++ src/zmq/astf/get_profile_list.mjs
@@ -6,11 +6,15 @@
 export async function get_profile_list({
   api_h = this.api_h,
   handler = this.port_handler, // Unique connection handler (from acquire())
+  port_id = 0,
+  profile_id = '*',
 } = {}) {
   // Send the request and wait for response
   const response = await this.send('get_profile_list', {
     api_h: api_h,
     handler: handler,
+    port_id: port_id,
+    profile_id: profile_id,
   });
   // Return the response
   return response;
